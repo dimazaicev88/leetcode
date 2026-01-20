e90db6cbda2b7b2715ee411e4068d29f4154bdf8
dima <dimazaicev88@gmail.com>
2026-01-20 10:42:04 +0300

    21

--- go/21/21.go
+++ go/21/21.go
@@ -9,56 +9,25 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil {
 		return list2
 	}
+
 	if list2 == nil {
 		return list1
 	}
 
 	var newList *ListNode
 
-	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
-			newList = &ListNode{
-				Val:  list1.Val,
-				Next: newList,
-			}
-		} else if list1.Val > list2.Val {
-			newList = &ListNode{
-				Val:  list2.Val,
-				Next: newList,
-			}
-		} else {
+	for list1 != nil || list2 != nil {
+		if list1 != nil {
 			listNode := &ListNode{Val: list1.Val, Next: newList}
 			newList = listNode
+			list1 = list1.Next
 		}
-		list1 = list1.Next
-		list2 = list2.Next
-	}
 
+		if list2 != nil {
+			listNode2 := &ListNode{Val: list2.Val, Next: newList}
+			newList = listNode2
+			list2 = list2.Next
+		}
+	}
 	return newList
-}
-
-func main() {
-
-	list1 := &ListNode{
-		Val: 1,
-		Next: &ListNode{
-			Val: 2,
-			Next: &ListNode{
-				Val: 3,
-			},
-		},
-	}
-
-	list2 := &ListNode{
-		Val: 1,
-		Next: &ListNode{
-			Val: 2,
-			Next: &ListNode{
-				Val: 3,
-			},
-		},
-	}
-
-	ml := mergeTwoLists(list1, list2)
-	fmt.Println(ml)
 }
---
+++ go/21/21_test.go
@@ -0,0 +1,63 @@
+package _21
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test21(t *testing.T) {
+
+	t.Run("case 1", func(t *testing.T) {
+		list1 := &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+				},
+			},
+		}
+
+		list2 := &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+				},
+			},
+		}
+
+		ml := mergeTwoLists(list1, list2)
+		for ml != nil {
+			fmt.Println(ml.Val)
+			ml = ml.Next
+		}
+	})
+
+	t.Run("case 1", func(t *testing.T) {
+		list1 := &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+			},
+		}
+
+		list2 := &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+				},
+			},
+		}
+
+		ml := mergeTwoLists(list1, list2)
+		for ml != nil {
+			fmt.Println(ml.Val)
+			ml = ml.Next
+		}
+	})
+
+}
